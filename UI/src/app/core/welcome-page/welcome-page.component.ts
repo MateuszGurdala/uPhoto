62a17c6308bb5b848c03e5cb715a6211936d6dcc
mateusz <mateusz.gurdala.mg@gmail.com>
2024-03-03 17:05:10 +0100

    Added welcome page

--- UI/src/app/core/welcome-page/welcome-page.component.ts
+++ UI/src/app/core/welcome-page/welcome-page.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
 import {NgOptimizedImage} from "@angular/common";
 import {TextColorReactDirective} from "../../shared/directives/text-color-react.directive";
+import {BackgroundColorReactDirective} from "../../shared/directives/background-color-react.directive";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, TextColorReactDirective],
+  imports: [HeaderComponent, NgOptimizedImage, TextColorReactDirective, BackgroundColorReactDirective, RouterLink],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
