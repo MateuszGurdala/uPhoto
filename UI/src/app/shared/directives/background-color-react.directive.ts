a333ebae2b078a762812f67c19c0d83de28e2015
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2024-03-04 22:02:25 +0100

    Added parameters to background color react directive

--- UI/src/app/shared/directives/background-color-react.directive.ts
+++ UI/src/app/shared/directives/background-color-react.directive.ts
@@ -1,12 +1,27 @@
-import {Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
-  selector: '[uBackgroundColorReact]',
-  standalone: true
+	selector: '[uBackgroundColorReact]',
+	standalone: true
 })
-export class BackgroundColorReactDirective {
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.classList.add('u-background-react');
-  }
+export class BackgroundColorReactDirective implements OnChanges {
+	@Input() disabled: boolean = false;
 
+	constructor(private element: ElementRef) {
+		this.element.nativeElement.classList.add('u-background-react');
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		this.disabled ? this.disable() : this.enable();
+	}
+
+	private enable(): void {
+		this.element.nativeElement.classList.remove('u-background-disabled');
+		this.element.nativeElement.classList.add('u-background-react');
+	}
+
+	private disable(): void {
+		this.element.nativeElement.classList.remove('u-background-react');
+		this.element.nativeElement.classList.add('u-background-disabled');
+	}
 }
--- UI/src/app/shared/directives/text-color-react.directive.ts
+++ UI/src/app/shared/directives/text-color-react.directive.ts
@@ -1,11 +1,35 @@
-import {Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[uTextColorReact]',
-  standalone: true
+	selector: '[uTextColorReact]',
+	standalone: true
 })
-export class TextColorReactDirective {
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.classList.add('u-color-react');
-  }
+export class TextColorReactDirective implements OnInit {
+	@Input() textBaseColor: string = '--accent';
+	@Input() textHoverColor: string = '--accent-tint';
+	@Input() textActiveColor: string = '--accent-shade';
+
+	constructor(private element: ElementRef, private renderer: Renderer2) {
+	}
+
+	ngOnInit(): void {
+		this.renderer.setStyle(this.element.nativeElement, 'color', `var(${this.textBaseColor})`);
+		this.renderer.setStyle(this.element.nativeElement, 'cursor', 'pointer');
+	}
+
+	@HostListener('mouseover') onMouseOver(): void {
+		this.renderer.setStyle(this.element.nativeElement, 'color', `var(${this.textHoverColor})`);
+	}
+
+	@HostListener('mouseleave') onMouseLeave(): void {
+		this.renderer.setStyle(this.element.nativeElement, 'color', `var(${this.textBaseColor})`);
+	}
+
+	@HostListener('mousedown') onMouseDown(): void {
+		this.renderer.setStyle(this.element.nativeElement, 'color', `var(${this.textActiveColor})`);
+	}
+
+	@HostListener('mouseup') onMouseUp(): void {
+		this.renderer.setStyle(this.element.nativeElement, 'color', `var(${this.textHoverColor})`);
+	}
 }
