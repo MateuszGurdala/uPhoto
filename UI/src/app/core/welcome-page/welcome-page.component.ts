d49b02f8a3d0df3d4407e54efb3cef5c413b14dd
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2024-03-05 18:00:40 +0100

    Misc

--- UI/src/app/core/welcome-page/welcome-page.component.ts
+++ UI/src/app/core/welcome-page/welcome-page.component.ts
@@ -1,16 +1,17 @@
+import { BackgroundColorReactDirective } from '../../shared/directives/background-color-react.directive';
 import { Component } from '@angular/core';
-import {HeaderComponent} from "../../shared/components/header/header.component";
-import {NgOptimizedImage} from "@angular/common";
-import {TextColorReactDirective} from "../../shared/directives/text-color-react.directive";
-import {BackgroundColorReactDirective} from "../../shared/directives/background-color-react.directive";
-import {RouterLink} from "@angular/router";
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { MatButton } from '@angular/material/button';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TextColorReactDirective } from '../../shared/directives/text-color-react.directive';
 
 @Component({
-  selector: 'app-welcome-page',
-  standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, TextColorReactDirective, BackgroundColorReactDirective, RouterLink],
-  templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.css'
+	selector: 'app-welcome-page',
+	standalone: true,
+	imports: [HeaderComponent, NgOptimizedImage, TextColorReactDirective, BackgroundColorReactDirective, RouterLink, MatButton],
+	templateUrl: './welcome-page.component.html',
+	styleUrl: './welcome-page.component.css'
 })
 export class WelcomePageComponent {
 
