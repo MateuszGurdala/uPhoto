e11334b4ff69f91b9da1a8d75ebb3f67a2e5287e
MateuszGurdala <mateusz.gurdala.mg@gmail.com>
2024-03-17 19:27:16 +0100

    Refactored app config + added HTTP providers

--- UI/src/app/app.config.ts
+++ UI/src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideServerRendering } from '@angular/platform-server';
+import { routes } from './app.routes';
+
+export const appClientConfig = {
+	apiUrl: 'http://localhost:5159/api/'
+};
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideClientHydration()]
+	providers: [
+		provideClientHydration(),
+		provideHttpClient(withFetch()),
+		provideRouter(routes),
+		provideServerRendering(),
+	]
 };
